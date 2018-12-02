4be4174bca3a35dbe89a172d4e92cbcb2cf1d92f
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-01 21:46:56 -0800

    fixed past events page

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ function handleClickOutsideNavbar(){
 
 function navbarGenerator(){
   $('.navbar').html(`
-  <a class="navbar-brand" href="http://myshalombayis.com"><img src="img/logo.png" width = "125px" height = "70px"></a>
+  <a class="navbar-brand" href="index.html#nav"><img src="img/logo.png" width = "125px" height = "70px"></a>
   <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -21,25 +21,23 @@ function navbarGenerator(){
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="http://myshalombayis.com/#about">About</a>
+        <a class="nav-link" href="index.html#about">About</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Events
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="http://myshalombayis.com/#upcoming_events">Upcoming Events</a>
+          <a class="dropdown-item" href="index.html#upcoming_events">Upcoming Events</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="http://myshalombayis.com/past_events.html">Past Events</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="http://myshalombayis.com/past_events.html">Gallery</a>
+          <a class="dropdown-item" href="past_events.html#nav">Past Events</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://myshalombayis.com/past_events.html#press">Press</a>
+        <a class="nav-link" href="past_events.html#press">Press</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://myshalombayis.com#subscribe">Subscribe</a>
+        <a class="nav-link" href="index.html#subscribe">Subscribe</a>
       </li>
       <li class="nav-item">
         <a class = "nav-link" href="mailto:info@myshalombayis.com?subject=Inquiry%20(From%20Website)&body=*Please%20include%20your%20name%20and%20number%20along%20with%20your%20inquiry.%20Thank%20you!"> Contact </a>
@@ -53,16 +51,16 @@ function footerGenerator(){
   $('#footer').html(`
   <div class = "row">
   <div class = "col-12">
-    <h6> <a href="http://myshalombayis.com">Shalom Bayis</a></h6>
+    <h6> <a href="index.html#nav">Shalom Bayis</a></h6>
   </div>
   <div class = "col-12">
     <p> 
-      <a href="http://myshalombayis.com/#about"> About | </a> 
-      <a href="http://myshalombayis.com/#upcoming_events"> Upcoming Events | </a> 
-      <a href="http://myshalombayis.com/past_events.html"> Past Events | </a> 
-      <a href="http://myshalombayis.com/past_events.html#press"> Press | </a>  
-      <a href="mailto:info@myshalombayis.com?subject=Inquiry%20(From%20Website)&body=*Please%20include%20your%20name%20and%20number%20along%20with%20your%20inquiry.%20Thank%20you!"> Contact | </a>
-      <a href="http://myshalombayis.com#subscribe"> Subscribe</a> 
+      <a href="index.html#about"> About | </a> 
+      <a href="index.html#upcoming_events"> Upcoming Events | </a> 
+      <a href="past_events.html#nav"> Past Events | </a> 
+      <a href="past_events.html#press"> Press | </a>  
+      <a href="index.html#subscribe"> Subscribe | </a> 
+      <a href="mailto:info@myshalombayis.com?subject=Inquiry%20(From%20Website)&body=*Please%20include%20your%20name%20and%20number%20along%20with%20your%20inquiry.%20Thank%20you!"> Contact</a>
     </p>
   </div>
 </div>
