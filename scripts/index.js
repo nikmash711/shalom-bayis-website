eee1f8e287ad34bd13f0f0bba77d8042eae3ab68
Nikkie Mashian <nikkie@tenantbase.com>
2022-03-25 08:18:46 -0700

    new flyer

--- scripts/index.js
+++ scripts/index.js
@@ -92,7 +92,7 @@ function upcomingFlyer() {
 }
 
 $(document).ready(function() {
-  upcomingEventTBA();
+//   upcomingEventTBA();
   // upcomingEventInfo();
-  // upcomingFlyer();
+  upcomingFlyer();
 });
